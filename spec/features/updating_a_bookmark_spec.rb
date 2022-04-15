5189a23c8d6d9aa84343e7e5443d2dc18b99e7d3
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-04-15 12:13:28 +0100

    Update bookmark views test drived. Need to update database

---
+++ spec/features/updating_a_bookmark_spec.rb
@@ -0,0 +1,23 @@
+# User story
+# As a user
+# So I can change a bookmark in Bookmark Manager
+# I want to update a bookmark
+
+feature 'Updating a bookmark' do
+  scenario 'A user can update a bookmark' do
+    bookmark = Bookmark.create(url: 'https://www.makersacademy.com', title: 'Makers Academy')
+    visit('bookmarks')
+    expect(page).to have_link('Makers Academy', href: 'https://www.makersacademy.com')
+
+    first('.bookmark').click_button 'Edit'
+    expect(current_path).to eq "/bookmarks/#{bookmark.id}/edit"
+
+    fill_in('url', with: "http://snakersacademy.com")
+    fill_in('title', with: "Snakers Academy")
+    click_button('Submit')
+
+    expect(current_path).to eq '/bookmarks'
+    expect(page).not_to have_link('Makers Academy', href: 'https://www.makersacademy.com')
+    expect(page).to have_link('Snakers Academy', href: 'https://www.snakersacademy.com')
+  end
+end
