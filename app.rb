dbca1676fb6006e046e7ef4a4aa8caa494af0cab
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-04-17 18:44:00 +0100

    User authentification happy path implemented

--- app.rb
+++ app.rb
@@ -90,5 +90,15 @@ class BookmarkManager < Sinatra::Base
     redirect '/bookmarks'
   end
 
+  get '/sessions/new' do
+    erb :"sessions/new"
+  end
+
+  post '/sessions' do
+    user = User.authenticate(email: params[:email], password: params[:password])
+    session[:user_id] = user.id
+    redirect '/bookmarks'
+  end
+
   run! if app_file == $0
 end
--- spec/units/user_spec.rb
+++ spec/units/user_spec.rb
@@ -27,9 +27,16 @@ describe '.find' do
     expect(result.email).to eq user.email
   end
 
-
-
   it 'returns nil if there is no ID given' do
     expect(User.find(nil)).to eq nil
+  end
+
+  describe '.authenticate' do
+    it 'returns a user given a correct username and password, if one exists' do
+      user = User.create(email: 'test@example.com', password: 'password123')
+      authenticated_user = User.authenticate(email: 'test@example.com', password: 'password123')
+
+      expect(authenticated_user.id).to eq user.id
+    end
   end
 end
